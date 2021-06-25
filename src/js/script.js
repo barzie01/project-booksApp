703a70d0722ee8bccd0cef356b969c498e06766a
Bartosz Ziętak <bartekzietak01@gmail.com>
2021-06-25 02:36:42 +0200

    Create favorite functionality. Start working on filter

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,7 @@
     },
     containerOf: {
       booksList: '.books-list',
+      form: '.filters form',
     },
     imageOf: {
       bookImage: '.book__image',
@@ -25,7 +26,11 @@
   const booksList = document.querySelector(select.containerOf.booksList);
   const books = dataSource.books;
   const favoriteBooks = [];
-  console.log(booksList);
+  // console.log(booksList);
+  const filters = [];
+  console.log(filters);
+  const form = document.querySelector(select.containerOf.form);
+  console.log(form.firstChild.tagName);
 
   function render() {
 
@@ -42,24 +47,48 @@
   render();
 
   function initActions () {
-    const bookImage = booksList.querySelectorAll(select.imageOf.bookImage);
-    console.log(bookImage);
 
-    for (let image of bookImage) {
+    booksList.addEventListener('dblclick', function(event) {
+      event.preventDefault();
 
-      // book = booksList.querySelector('.book');
-      image.addEventListener('dblclick', function(event) {
-        event.preventDefault();
+      // console.log(event.target.offsetParent);
 
-        image.classList.add(classNames.books.favoriteBook);
+      if(!event.target.offsetParent.classList.contains(classNames.books.favoriteBook)){
+
+        event.target.offsetParent.classList.add(classNames.books.favoriteBook);
 
         // get bookId of data-id
-        const bookId = image.getAttribute('data-id');
+        const bookId = event.target.offsetParent.getAttribute('data-id');
 
         // add bookId to favoriteBooks
         favoriteBooks.push(bookId);
-      });
-    }
+      } else {
+        const indexOfBookId = favoriteBooks.indexOf('data-id');
+        favoriteBooks.splice(indexOfBookId, 1);
+
+        event.target.offsetParent.classList.remove(classNames.books.favoriteBook);
+      }
+
+    });
+
+    form.addEventListener('click', function(event){
+
+      // console.log(event.target.tagName);
+
+      if(event.target.tagName == 'INPUT' && event.target.type == 'checkbox' && event.target.name == 'filter'){
+        console.log(event.target.value);
+      }
+
+      if(event.target.checked == true){
+        filters.push(event.target.value);
+
+      } else {
+        const valueIndex = filters.indexOf(event.target.value);
+        filters.splice(valueIndex, 1);
+      }
+      console.log(filters);
+    });
   }
   initActions();
 }
+
